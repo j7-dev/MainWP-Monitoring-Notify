349cf2dc84312822087941c6041f448322388bc0
J7 <j7.dev.gg@outlook.com>
2023-05-25 09:33:43 +0800

    feat: add library kittinan/php-line-notify

--- mainwp-monitoring-notify.php
+++ mainwp-monitoring-notify.php
@@ -9,226 +9,100 @@
  * Author URI: https://github.com/j7-dev
  * Documentation URI:
  */
+require __DIR__ . '/vendor/autoload.php';
 
-if (!defined('MAINWP_BRANDING_PLUGIN_FILE')) {
-	define('MAINWP_BRANDING_PLUGIN_FILE', __FILE__);
-}
-
-class MainWP_Branding_Extension
+class MainWP_Monitoring_Notify_Extension
 {
-	public static $instance = null;
-	public $plugin_handle = 'mainwp-branding-extension';
-	public $plugin_slug;
-	protected $plugin_url;
+	public static $childKey = false;
+	public $token = '3qXsuC4zQ7V0BdlamXpDBHZeeZ8AQOBLQdEgoOtoHwx';
+
 
 	public function __construct()
 	{
-
-		$this->plugin_url  = plugin_dir_url(__FILE__);
-		$this->plugin_slug = plugin_basename(__FILE__);
-
-		add_action('admin_init', array(&$this, 'admin_init'));
-		add_filter('plugin_row_meta', array(&$this, 'plugin_row_meta'), 10, 2);
-		add_action('mainwp_delete_site', array(&$this, 'on_delete_site'), 10, 1);
-		add_filter('mainwp_getsubpages_sites', array(&$this, 'managesites_subpage'), 10, 1);
-		add_filter('mainwp_sync_extensions_options', array(&$this, 'mainwp_sync_extensions_options'), 10, 1);
-		add_action('init', array(&$this, 'localization'));
-		add_action('mainwp_applypluginsettings_mainwp-branding-extension', array(MainWP_Branding::get_instance(), 'mainwp_apply_plugin_settings'));
-		MainWP_Branding_DB::get_instance()->install();
-		MainWP_Branding::get_instance()->init();
+		add_action('init', [$this, 'initialize']);
+		add_filter('mainwp-getextensions', [$this, 'get_this_extension']);
+		add_action('mainwp_after_notice_sites_uptime_monitoring_admin', [$this, 'handle_offline_site']);
+		// add_action('wp_head',  [$this, 'test']);
 	}
 
-	static function get_instance()
+	public function initialize()
 	{
+		$mainWPActivated = apply_filters('mainwp-activated-check', false);
 
-		if (null == self::$instance) {
-			self::$instance = new MainWP_Branding_Extension();
-		}
-
-		return self::$instance;
-	}
-
-	public function localization()
-	{
-		load_plugin_textdomain('mainwp-branding-extension', false,  dirname(plugin_basename(__FILE__)) . '/languages/');
-	}
-
-	public function plugin_row_meta($plugin_meta, $plugin_file)
-	{
-
-		if ($this->plugin_slug != $plugin_file) {
-			return $plugin_meta;
-		}
-
-		$slug     = basename($plugin_file, '.php');
-		$api_data = get_option($slug . '_APIManAdder');
-		if (!is_array($api_data) || !isset($api_data['activated_key']) || $api_data['activated_key'] != 'Activated' || !isset($api_data['api_key']) || empty($api_data['api_key'])) {
-			return $plugin_meta;
-		}
-
-		$plugin_meta[] = '<a href="?do=checkUpgrade" title="Check for updates.">Check for updates now</a>';
-
-		return $plugin_meta;
-	}
-
-	public function on_delete_site($website)
-	{
-		if ($website) {
-			MainWP_Branding_DB::get_instance()->delete_branding($website->id);
-		}
-	}
-
-	public function mainwp_sync_extensions_options($values = array())
-	{
-		$values['mainwp-branding-extension'] = array(
-			'plugin_slug' => null,
-		);
-		return $values;
-	}
-
-	public function managesites_subpage($subPage)
-	{
-		$subPage[] = array(
-			'title'            => __('White Label', 'mainwp'),
-			'slug'             => 'WhiteLabel',
-			'sitetab'          => true,
-			'menu_hidden'      => true,
-			'callback'         => array('MainWP_Branding', 'render'),
-			'on_load_callback' => array('MainWP_Branding', 'on_load_page'),
-		);
-		return $subPage;
-	}
-
-	public function admin_init()
-	{
-
-		$output = MainWP_Branding::handle_settings_post();
-
-		if (false !== $output) {
-			$referer  = wp_get_referer();
-			wp_redirect(add_query_arg(array('updated' => true), $referer));
-			exit();
-		}
-
-		wp_enqueue_style('mainwp-branding-extension', $this->plugin_url . 'css/mainwp-branding.css');
-		wp_enqueue_script('mainwp-branding-extension', $this->plugin_url . 'js/mainwp-branding.js', array(), '1.2');
-	}
-}
-
-
-function mainwp_branding_extension_autoload($class_name)
-{
-
-	$allowedLoadingTypes = array('class', 'page');
-	$class_name = str_replace('_', '-', strtolower($class_name));
-	foreach ($allowedLoadingTypes as $allowedLoadingType) {
-		$class_file = WP_PLUGIN_DIR . DIRECTORY_SEPARATOR . str_replace(basename(__FILE__), '', plugin_basename(__FILE__)) . $allowedLoadingType . DIRECTORY_SEPARATOR . $class_name . '.' . $allowedLoadingType . '.php';
-		if (file_exists($class_file)) {
-			require_once $class_file;
-		}
-	}
-}
-
-class MainWP_Branding_Extension_Activator
-{
-	protected $mainwpMainActivated = false;
-	protected $childEnabled = false;
-	protected $childKey = false;
-	protected $childFile;
-	protected $plugin_handle = 'mainwp-branding-extension';
-	protected $product_id = 'MainWP Branding Extension';
-	protected $software_version = '4.1.3';
-
-	public function __construct()
-	{
-
-		$this->childFile = __FILE__;
-
-		$this->includes();
-		register_activation_hook(__FILE__, array($this, 'activate'));
-		register_deactivation_hook(__FILE__, array($this, 'deactivate'));
-
-		add_filter('mainwp_getextensions', array(&$this, 'get_this_extension'));
-		$this->mainwpMainActivated = apply_filters('mainwp_activated_check', false);
-
-		if (false !== $this->mainwpMainActivated) {
-			$this->activate_this_plugin();
+		if ($mainWPActivated !== false) {
+			self::activate_this_plugin();
 		} else {
-			add_action('mainwp_activated', array(&$this, 'activate_this_plugin'));
+			add_action('mainwp-activated', 'activate_this_plugin');
 		}
-		add_action('admin_notices', array(&$this, 'mainwp_error_notice'));
 	}
 
-	public function includes()
+	public function test()
 	{
-		include_once 'class/mainwp-branding-db.class.php';
-		include_once 'class/mainwp-branding.class.php';
+		$websites = MainWP\Dashboard\MainWP_DB::instance()->query(MainWP\Dashboard\MainWP_DB::instance()->get_sql_websites_for_current_user());
+		// $offlineSites = array_filter($websites, [$this, "filter_offline_site_callback"]);
+		var_dump($websites);
 	}
 
-	public function activate_this_plugin()
+	public function get_this_extension($extensions)
 	{
-		$this->mainwpMainActivated = apply_filters('mainwp_activated_check', $this->mainwpMainActivated);
-		$this->childEnabled = apply_filters('mainwp_extension_enabled_check', __FILE__);
-		$this->childKey = $this->childEnabled['key'];
-		if (function_exists('mainwp_current_user_can') && !mainwp_current_user_can('extension', 'mainwp-branding-extension')) {
-			return;
-		}
-		new MainWP_Branding_Extension();
-	}
-
-	public function get_this_extension($pArray)
-	{
-		$pArray[] = array(
-			'plugin'     				=> __FILE__,
-			'api'        				=> $this->plugin_handle,
-			'mainwp'     				=> true,
-			'callback'   				=> array(&$this, 'settings'),
-			'apiManager' 				=> true,
-			'on_load_callback' => array('MainWP_Branding', 'on_load_page'),
+		$extensions[] = array(
+			'plugin' => __FILE__,
+			'callback' => [$this, 'plugin_extension_settings'],
+			'apiManager' => false,
+			'name' => 'Monitoring Notify',
 		);
-
-		return $pArray;
+		return $extensions;
 	}
 
-	public function settings()
+	public function plugin_extension_settings()
 	{
-		do_action('mainwp_pageheader_extensions', __FILE__);
-		MainWP_Branding::render();
-		do_action('mainwp_pagefooter_extensions', __FILE__);
+		do_action('mainwp-pageheader-extensions', __FILE__);
+?>
+		<h1>123156</h1>
+<?php
+		do_action('mainwp-pagefooter-extensions', __FILE__);
 	}
 
-	public function mainwp_error_notice()
+	public function handle_offline_site($websites)
 	{
-		global $current_screen;
-		if ($current_screen->parent_base == 'plugins' && $this->mainwpMainActivated == false) {
-			echo '<div class="error"><p>MainWP White Label Extension ' . __('requires <a href="https://mainwp.com/" target="_blank">MainWP Dashboard plugin</a> to be activated in order to work. Please install and activate <a href="https://mainwp.com/" target="_blank">MainWP Dashboard plugin</a> first.') . '</p></div>';
+		$offlineSites = array_filter($websites, [$this, "filter_offline_site_callback"]);
+		$msg = '';
+		if (empty($offlineSites)) {
+			$msg .= "✅ 檢查所有網站都正常運作中\n";
+		} else {
+			foreach ($offlineSites as $site) {
+				$code = $site->http_response_code;
+				$code_string = MainWP\Dashboard\MainWP_Utility::get_http_codes($code);
+				if (!empty($code_string)) {
+					$code .= ' - ' . $code_string;
+				}
+				$msg .= "⚠️ 偵測到網站異常" . $site->name  . '  🔴' . $code  . "\n";
+				$msg .= "請確認並聯繫網站管理員\n";
+				$msg .= $site->url . "\n\n";
+			}
 		}
+
+		$ln = new KS\Line\LineNotify(self::$token);
+		$ln->send($msg);
 	}
 
-	public function get_child_key()
+	public function filter_offline_site_callback($website)
 	{
-		return $this->childKey;
+		return $website->http_response_code !== "200";
 	}
 
-	public function get_child_file()
-	{
-		return $this->childFile;
-	}
 
-	public function activate()
-	{
-		$options = array(
-			'product_id'       => $this->product_id,
-			'software_version' => $this->software_version,
-		);
-		do_action('mainwp_activate_extension', $this->plugin_handle, $options);
-	}
 
-	public function deactivate()
+	public static function activate_this_plugin()
 	{
-		do_action('mainwp_deactivate_extension', $this->plugin_handle);
+		global $childEnabled;
+		$childEnabled = apply_filters('mainwp-extension-enabled-check', __FILE__);
+		if (!$childEnabled) return;
+
+		$childKey = $childEnabled['key'];
+
+		//Code to initialize your plugin
 	}
 }
 
-global $mainWPBrandingExtensionActivator;
-$mainWPBrandingExtensionActivator = new MainWP_Branding_Extension_Activator();
+new MainWP_Monitoring_Notify_Extension();
