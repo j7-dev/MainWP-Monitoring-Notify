b8309c06651ef879ca5ffd0a10cf7cf2cd08b3a0
J7 <j7.dev.gg@outlook.com>
2023-05-26 00:55:24 +0800

    feat: add helper image

---
+++ assets/js/main.js
@@ -0,0 +1,53 @@
+(function ($) {
+  init();
+
+  function init() {
+    waitRendered("#monitoring_notify_submit_btn", function () {
+      const saveBtn = $(this);
+
+      saveBtn.click(function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        saveBtn.addClass("loading");
+        updateSettings();
+      });
+    });
+  }
+
+  function updateSettings() {
+    const saveBtn = $("#monitoring_notify_submit_btn");
+    const responseMsg = $("#response_msg");
+    const data = {
+      action: info.action,
+      nonce: info.nonce,
+      mainwp_monitoring_notify_line_token: $(
+        "#mainwp_monitoring_notify_line_token"
+      ).val(),
+    };
+    $.post(info?.ajax_url, data, function (response) {
+      saveBtn.removeClass("loading");
+      if (response?.status === "success") {
+        responseMsg.html(renderSuccessMessage());
+      } else {
+        responseMsg.html(renderErrorMessage());
+        console.log(response);
+      }
+    });
+  }
+
+  function waitRendered(selector, callback) {
+    const interval = setInterval(function () {
+      if ($(selector).length > 0) {
+        callback.bind($(selector))();
+        clearInterval(interval);
+      }
+    }, 300);
+  }
+
+  function renderSuccessMessage(message = "保存成功") {
+    return `<div class="ui green check icon message">${message}</div>`;
+  }
+  function renderErrorMessage(message = "保存失敗，請稍後再試或洽管理員") {
+    return `<div class="ui red close icon message">${message}</div>`;
+  }
+})(jQuery);
