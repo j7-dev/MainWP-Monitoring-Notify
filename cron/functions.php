efb705db0b74887c7d29e0cd4c8f4d837629eba0
J7 <j7.dev.gg@outlook.com>
2023-05-27 02:03:08 +0800

    feat: get_sites and get_site_urls

--- cron/functions.php
+++ cron/functions.php
@@ -9,7 +9,29 @@ function get_http_status_code($url)
 	return $status_code;
 }
 
+function get_sites()
+{
+	global $wpdb;
+	$table_name = $wpdb->prefix . 'mainwp_wp';
+
+	$results = $wpdb->get_results("SELECT name, url FROM $table_name"); // 選取 name 和 url 欄位
+
+	return $results;
+}
+
+function get_site_urls()
+{
+	$sites = get_sites();
+	$site_urls = [];
+	foreach ($sites as $site) {
+		$site_urls[] = $site->url;
+	}
+
+	return $site_urls;
+}
+
 
 // $url = "https://test.yc-tech.co/";
 // $status_code = get_http_status_code($url);
-// var_dump($status_code === '200');
+
+var_dump(get_site_urls());
